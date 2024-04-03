05b346bf6ddaa916de0acc76f2a1c5439eba7531
Nibhafathima <151762426+Nibhafathima@users.noreply.github.com>
2024-04-03 14:00:30 +0530

    Create palindrom.sh

---
+++ palindrom.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read a
+
+reverse=0
+temp=$a
+
+while [ $temp -gt 0 ]
+do
+    digit=$(( $temp % 10 ))
+    temp=$(( temp / 10 ))
+    reverse=$(( $reverse * 10 + $digit ))
+done
+
+if [ $a -eq $reverse ]
+then
+    echo "The given number is a palindrome."
+else
+    echo "The given number is not a palindrome."
+fi
