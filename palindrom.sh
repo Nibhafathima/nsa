9e9e3662028deb17da737e56185cf0c425fb8836
Nibhafathima <151762426+Nibhafathima@users.noreply.github.com>
2024-04-29 22:34:07 +0530

    Update palindrom.sh

--- palindrom.sh
+++ palindrom.sh
@@ -8,9 +8,9 @@ temp=$a
 
 while [ $temp -gt 0 ]
 do
-    digit=$(( $temp % 10 ))
+    digit=$(( temp % 10 ))
     temp=$(( temp / 10 ))
-    reverse=$(( $reverse * 10 + $digit ))
+    reverse=$(( reverse * 10 + $digit ))
 done
 
 if [ $a -eq $reverse ]
