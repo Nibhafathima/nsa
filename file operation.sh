0476067c40de699fba0c55f25a27a630d05f1738
Nibhafathima <151762426+Nibhafathima@users.noreply.github.com>
2024-06-11 11:58:05 +0530

    Create file operation.sh

---
+++ file operation.sh
@@ -0,0 +1,9 @@
+echo "Enter the name of your file: "
+read file
+c=$(cat "$file" | wc -c)
+w=$(cat "$file" | wc -w)
+l=$(grep -c "." "$file")
+echo "Number of characters in $file: $c"
+echo "Number of words in $file: $w"
+echo "Number of lines in $file: $l"
+
